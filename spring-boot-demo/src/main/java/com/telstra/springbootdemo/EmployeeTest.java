6a3becc7b9b4b6387dbbb917abcad032325847e6
VadagamSaiKrupa <90339181+VadagamSaiKrupa@users.noreply.github.com>
2023-08-08 09:40:15 +0530

    Add files via upload

---
+++ spring-boot-demo/src/main/java/com/telstra/springbootdemo/EmployeeTest.java
@@ -0,0 +1,19 @@
+package com.telstra.springbootdemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest {
+
+	
+	@Test
+	public void getSalaryTest() {
+		Employee employee = new Employee(1001,"Ram","Manager",50000);
+		assertEquals(50000,employee.getSalary());
+	}
+	
+	@Test
+	public void getNameTest() {
+		Employee employee = new Employee(1001,"Ram","Manager",50000);
+		assertEquals("Ram",employee.getName());
+	}
+}
